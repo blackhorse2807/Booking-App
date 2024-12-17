4206773b9bef396edf181a10c2333001220b21b3
Ujjwal Kumar Singh <54690814+blackhorse2807@users.noreply.github.com>
2024-12-17 19:23:33 +0530

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse, NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // Skip middleware for NextAuth routes and static files
+  if (
+    pathname.startsWith("/api/auth") || 
+    pathname.startsWith("/") ||
+    pathname === "/" ||
+    pathname.startsWith("/_next") || 
+    pathname.startsWith("/favicon.ico")
+  ) {
+    return NextResponse.next();
+  }
+
+  // Check for authentication token
+  const token = await getToken({
+    req,
+    secret: "YQrhB4iVXsWkfYmdkCFq80JKEmPOfeN0JCipfa9DFMg=", // Use your secure secret
+  });
+
+  console.log("Middleware Token:", token); // Debugging output
+
+  const protectedRoutes = ["/placeorder", "/booking-overview"];
+  const dynamicRoutePattern = /^\/details\/\d+$/;
+  // Redirect unauthenticated users trying to access protected routes
+  if (!token && (protectedRoutes.some(route => pathname.startsWith(route)) || dynamicRoutePattern.test(pathname))) {
+    return NextResponse.redirect(new URL("/signup", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/home", "/placeorder", "/booking-overview", "/details/:id*"], // Protect these paths
+  };
